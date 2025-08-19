250ff379d8ed4144bff0728ef61d8db93324ef22
ITS-GK03 <kggowtham1803@gmail.com>
2025-08-19 11:37:19 +0530

    first commit

--- Modulus.py
+++ Modulus.py
@@ -1,9 +1,26 @@
-from Calculator import*
+# from Calculator import*
 
-cal_add(3,7)
+# cal_add(3,7)
 
-cal_sub(7,3)
+# cal_sub(7,3)
 
-cal_mul(9,9)
+# cal_mul(9,9)
 
-cal_div(9,3)
+# cal_div(9,3)
+
+
+# import math 
+
+# print(math.copysign(3.18,-7.3))
+
+# print(math.factorial(7))
+
+# print(math.floor(7.89))
+
+# print(math.gamma(7))
+
+# print(math.trunc(18.99))
+
+# print(math.sqrt(21))
+
+# print(math.)
