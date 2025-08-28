a8dbc758e5928613950f1b67da3e1cba65ac24c0
ITS-GK03 <kggowtham1803@gmail.com>
2025-08-28 12:16:28 +0530

    first commit

---
+++ OOPS/Class_and_object.py
@@ -0,0 +1,105 @@
+# oops - object oriented programming system:
+
+# CLASS - OBJECT:
+
+
+# 1.create class Goa . Function as party , beach:
+
+# class Goa:
+#     fun_activities=""
+#     party_place=""
+#     age=0
+
+#     def party(self):
+#         print("Such a happrning place a full fun and party mode on and a boys trip bucket list place is goa........")
+    
+#     def beach(self):
+#         print("Beach is full of water activities and fun filled combo")
+    
+
+# goa1=Goa()
+# goa1.fun_activities="Water sliding"
+# goa1.age=20
+# print(goa1.fun_activities)
+# print(goa1.age)
+
+# goa1.beach()
+
+# print("\n")
+# goa2=Goa()
+# goa2.party_place="Beach house"
+# goa2.age=22
+# print(goa2.party_place)
+# print(goa2.age)
+
+# goa2.party()
+
+
+
+# 2. Create a  class called laptop. Create  the following variables and functions . 
+#   		Var = price, processor, ram
+# Create object as Lenovo, HP, Dell
+
+# class Laptop:
+#     price_lap=0
+#     processor_lap=""
+#     ram_lap=0
+
+#     def lenovo(self):
+#         print("Lenovo has a best processoor aand ram in lapton but it has issue of price..... ")
+    
+#     def hp(self):
+#         print("HP has best in all it has no issues abouyt laptop.....")
+
+#     def dell(self):
+#         print("Dell has best ram and price but it has heating isses ")
+
+
+# laptop1=Laptop()
+# laptop1.price_lap=55000
+# laptop1.processor_lap="Intel i12"
+# laptop1.ram_lap=16
+# print(laptop1.price_lap)
+# print(laptop1.processor_lap)
+# print(laptop1.ram_lap)
+
+# laptop1.lenovo()
+
+# print("\n")
+# laptop2=Laptop()
+# laptop2.price_lap=60000
+# laptop2.processor_lap="Intel i14"
+# laptop2.ram_lap=16
+# print(laptop2.price_lap)
+# print(laptop2.processor_lap)
+# print(laptop2.ram_lap)
+
+# laptop2.hp()
+
+# print("\n")
+# laptop3=Laptop()
+# laptop3.price_lap=40000
+# laptop3.processor_lap="Intel i12"
+# laptop3.ram_lap=8
+# print(laptop3.price_lap)
+# print(laptop3.processor_lap)
+# print(laptop3.ram_lap)
+
+# laptop3.lenovo()
+
+
+
+# 3.create class called Kodaikanal.and create all the possible variables and functions:
+
+# class Kodaikanal:
+#     def __init__(self, place1, place2):
+#         self.place1=place1
+#         self.place2=place2
+    
+#     def getter_fun(self):
+#         print("Place 1:", self.place1)
+#         print("Place 2:", self.place2)
+
+# kodai1=Kodaikanal("Guna Cave", "Pine forest")
+
+# kodai1.getter_fun()
